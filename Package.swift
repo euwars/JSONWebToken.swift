f71f1f13d456adb95dd5465a94d0701cc43bc0c4
Farzad <nazifi10@gmail.com>
2017-11-29 03:30:40 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -5,6 +5,7 @@ import PackageDescription
 let package = Package(
   name: "JWT",
   dependencies: [
+    .Package(url: "https://github.com/IBM-Swift/CommonCrypto.git", majorVersion: 0, minor: 1),
   ],
   exclude: [
     "Sources/JWT/HMACCryptoSwift.swift",
